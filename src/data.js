21f2c079dd87a605ab019f17a0268b06e1b5a707
eagarer <erayagarer@gmail.com>
2022-09-15 23:14:07 +0300

    Added product list

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://i.pngimg.me/thumb/f/720/3b9d383650924372aa3b.jpg",
+    img: "https://cdn.sorsware.com/oxxo/ContentImages/Product/22k/22KOX-PAMTEKMIN/nakis-detayli-mini-etek_antik-olive-darb-yesil_1_buyuk.jpg",
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
   },
   {
     id: 2,
-    img: "https://i.pngimg.me/thumb/f/720/3b9d383650924372aa3b.jpg",
+    img: "https://images.unsplash.com/photo-1523359346063-d879354c0ea5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
     title: "WINTER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "https://i.pngimg.me/thumb/f/720/3b9d383650924372aa3b.jpg",
+    img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=420&q=80",
     title: "POPULAR SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fbf0f4",
@@ -25,17 +25,52 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "https://www.bodenimages.com/productimages/productxlarge/22waut_t0896_wht.jpg",
+    img: "https://cdn.sorsware.com/oxxo/ContentImages/Product/21k/21KOX-EKOCEKGOM/ekoseli-gomlek-ceket_cerulean-check-mavi_1_buyuk.jpg",
     title: "SHIRT STYLE!",
   },
   {
     id: 2,
-    img: "https://www.bodenimages.com/productimages/productxlarge/22waut_j0178_ind.jpg",
+    img: "https://cdn.sorsware.com/oxxo/ContentImages/Product/22k/22KOX-CUPNEWTOP/soft-touch-sifir-yaka-tisort-modal-_black-siyah_1_buyuk.jpg",
     title: "LOUNGEWEAR LOVE",
   },
   {
     id: 3,
     img: "https://www.bodenimages.com/productimages/productxlarge/22wwin_u0066_blk.jpg",
     title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://www.prada.com/content/dam/pradanux_products/1/1HC/1HC551/2D0KF063R/1HC551_2D0K_F063R_SLF.png/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.square.jpg",
+  },
+  {
+    id: 2,
+    img: "https://www.prada.com/content/dam/pradabkg_products/1/1HC/1HC137/2DMIF0002/1HC137_2DMI_F0002_SLF.jpg/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.crop.jpg",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradabkg_products/1/1PS/1PS057/66AF0Y3N/1PS057_66A_F0Y3N_SLF.jpg/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.crop.jpg",
+  },
+  {
+    id: 4,
+    img: "https://www.prada.com/content/dam/pradabkg_products/1/1MV/1MV021/QHHF0458/1MV021_QHH_F0458_SLF.jpg/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.crop.jpg",
+  },
+  {
+    id: 5,
+    img: "https://www.prada.com/content/dam/pradabkg_products/1/1HC/1HC274/AJ6F0008/1HC274_AJ6_F0008_SLF.jpg/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.crop.jpg",
+  },
+  {
+    id: 6,
+    img: "https://www.prada.com/content/dam/pradanux_products/1/1MC/1MC211/2DYGF0002/1MC211_2DYG_F0002_SLF.png/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.square.jpg",
+  },
+  {
+    id: 7,
+    img: "https://www.prada.com/content/dam/pradanux_products/1/1GG/1GG141/2DWZF0106/1GG141_2DWZ_F0106_SLF.png/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.square.jpg",
+  },
+  {
+    id: 8,
+    img: "https://www.prada.com/content/dam/pradanux_products/1/1GG/1GG144/2DWZF0002/1GG144_2DWZ_F0002_SLF.png/jcr:content/renditions/cq5dam.web.hebebed.1000.1000.square.jpg",
   },
 ];
