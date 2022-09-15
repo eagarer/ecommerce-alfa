2075f9199c9762084898e00e6da8968711e4f85a
eagarer <erayagarer@gmail.com>
2022-09-15 16:26:58 +0300

    Added categories

--- src/data.js
+++ src/data.js
@@ -1,23 +1,41 @@
 export const sliderItems = [
-    {
-        id:1,
-        img:"https://i.pngimg.me/thumb/f/720/3b9d383650924372aa3b.jpg",
-        title:"SUMMER SALE",
-        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg:"f5fafd",
-    },
-    {
-        id:2,
-        img:"https://i.pngimg.me/thumb/f/720/3b9d383650924372aa3b.jpg",
-        title:"WINTER SALE",
-        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg:"fcf1ed",
-    },
-    {
-        id:3,
-        img:"https://i.pngimg.me/thumb/f/720/3b9d383650924372aa3b.jpg",
-        title:"POPULAR SALE",
-        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg:"fbf0f4",
-    }
-]
+  {
+    id: 1,
+    img: "https://i.pngimg.me/thumb/f/720/3b9d383650924372aa3b.jpg",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.pngimg.me/thumb/f/720/3b9d383650924372aa3b.jpg",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.pngimg.me/thumb/f/720/3b9d383650924372aa3b.jpg",
+    title: "POPULAR SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://www.bodenimages.com/productimages/productxlarge/22waut_t0896_wht.jpg",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://www.bodenimages.com/productimages/productxlarge/22waut_j0178_ind.jpg",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://www.bodenimages.com/productimages/productxlarge/22wwin_u0066_blk.jpg",
+    title: "LIGHT JACKETS",
+  },
+];
