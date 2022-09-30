6bdc6fabbc5401a53e23a7019ddb591606e759a4
eagarer <erayagarer@gmail.com>
2022-09-30 12:40:22 +0300

    Added react stripe payment implementation

--- src/pages/Pay.jsx
+++ src/pages/Pay.jsx
@@ -1,12 +1,35 @@
+/* eslint-disable no-unused-expressions */
 import StripeCheckout from "react-stripe-checkout";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const KEY =
   "pk_test_51Lma2zFcv0k9aeTH84Xnz6mv4wdqK716q2YAwoSaHSvzLSaZnPAtNgmgMrgsHCdKlEPEdmEASBUOYGlpOxddkEs800dFH8qd5u";
 
 const Pay = () => {
-    const onToken =(token)=> {
-        console.log(token)
-    }
+  const [stripeToken, setStripeToken] = useState(null);
+
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await axios.post(
+          "http://localhost:5000/api/checkout/payment",
+          {
+            tokenId: stripeToken.id,
+            amount: 2000,
+          }
+        );
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken]);
   return (
     <div
       style={{
@@ -36,6 +59,8 @@ const Pay = () => {
             color: "white",
             fontWeight: "600",
             cursor: "pointer",
+            alignItem: "center",
+            
           }}
         >
           Pay Now
